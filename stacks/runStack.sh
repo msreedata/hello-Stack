45cd1d65f89a3586d86b60b37ab3260a09cc12a0
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-11 09:25:58 +0200

    stack build tested.

--- stacks/runStack.sh
+++ stacks/runStack.sh
@@ -2,3 +2,8 @@
 sudo docker swarm init
 
 sudo docker stack deploy --compose-file ./stacks/Stackfile-hello.yml hellov2
+
+#view stack details
+sudo docker stack ls
+sudo docker service ls
+sudo docker container ps
