ae948964284204b8bf4e19d9583b4adf4355b8a7
Sreejith (on HVM) <sreejithsm@gmail.com>
2018-06-11 14:13:12 +0200

    ..

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-pushd $HOME/dockerstore/hello-Stack
+if [[ $projectDir && -d $projectDir ]];then
+    pushd $projectDir #$HOME/dockerstore/hello-Stack
 
-#build php
-sudo docker build ./hello-Node -t hellonode:v2
-sudo docker build ./hello-Php -t hellophp:v2
-sudo docker build ./hello-Go -t hellogo:v2
+    #build php
+    sudo docker build ./hello-Node -t hellonode:v2
+    sudo docker build ./hello-Php -t hellophp:v2
+    sudo docker build ./hello-Go -t hellogo:v2
 
-popd 
+    popd 
 
-sudo docker image ls
+    sudo docker image ls
+else
+    echo "...skipping build."
+fi
