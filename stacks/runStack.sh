885889597693a64af14b0b60c12c0c1cdfc75d81
Sreejith (on HVM) <sreejithsm@gmail.com>
2018-06-11 16:49:25 +0200

    ..

--- stacks/runStack.sh
+++ stacks/runStack.sh
@@ -4,11 +4,11 @@ if [[ $projectDir && -d $projectDir ]];then
 sudo docker swarm init
 sudo docker stack rm $stackName
 
-until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -lt 0 ]; do
+until [ -z "$(sudo docker service ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -lt 0 ]; do
   sleep 1;
 done
 
-until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -lt 0 ]; do
+until [ -z "$(sudo docker network ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -lt 0 ]; do
   sleep 1;
 done
 
