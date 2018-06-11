04cebdea367d425f82bcca04cd7b0deddf3cef9f
Sreejith (on HVM) <sreejithsm@gmail.com>
2018-06-11 14:33:15 +0200

    ..adding no cache for force build

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -3,9 +3,9 @@ if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/hello-Stack
 
     #build php
-    sudo docker build ./hello-Node -t hellonode:v2
-    sudo docker build ./hello-Php -t hellophp:v2
-    sudo docker build ./hello-Go -t hellogo:v2
+    sudo docker build --no-cache ./hello-Node -t hellonode:v2
+    sudo docker build --no-cache ./hello-Php -t hellophp:v2
+    sudo docker build --no-cache ./hello-Go -t hellogo:v2
 
     popd 
 
