cdda26d5c52844d887dbc43632f55d2ee4780973
Sreejith (on HVM) <sreejithsm@gmail.com>
2018-06-12 14:40:37 +0200

    ..

--- stacks/runStack.sh
+++ stacks/runStack.sh
@@ -9,12 +9,14 @@ declare -i limit=60
 until [ -z "$(sudo docker service ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -le 0 ]; do
   sleep 1;
   ((limit--))
+  echo "wait for clearing service... ${limit}"
 done
 
 limit=60
 until [ -z "$(sudo docker network ls --filter label=com.docker.stack.namespace=$stackName -q)" ] || [ "$limit" -lt 0 ]; do
   sleep 1;
   ((limit--))
+  echo "wait for clearing network... ${limit}"
 done
 
 sudo docker stack deploy --compose-file $projectDir/stacks/Stackfile-hello.yml $stackName
