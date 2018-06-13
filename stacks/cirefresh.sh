6ac570dd090b0fe4219313ddb7541b751b0ed9d6
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-13 22:53:37 +0200

    addign trusted cert to ciRefresh

--- stacks/cirefresh.sh
+++ stacks/cirefresh.sh
@@ -4,6 +4,9 @@
 #This will be already done by CI #. ./stacks/initRepo.sh
 projectDir=$(pwd)
 
+#install trusted certificate
+sudo cp ./stacks/linBox.crt /etc/docker/certs.d/linBox:5000/ca.crt
+
 #build latest docker image for testing
 echo "Project path :  $projectDir"
 sudo docker build --no-cache ./hello-Node -t linBox:5000/hellonode:v2
--- stacks/steps-SelfCertCreation.sh
+++ stacks/steps-SelfCertCreation.sh
@@ -1,14 +1,18 @@
 #linBox CN
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/linBox.key -x509 -days 365 -out ~/keys/linBox.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=linBox.com"
+rm ~/keys/* -rf
+mkdir ~/keys
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/linBox.key -x509 -days 365 -out ~/keys/linBox.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=linBox"
 #upload the files to azure #registy.smlabs.com
 scp ~/keys/linBox.crt ~/keys/linBox.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+#also copy to local
+pscp LinuxAdmin@smlin.westeurope.cloudapp.azure.com:/home/LinuxAdmin/keys/* "D:\1.DataStore\Documents\1.E-Cert.s\eCertificates & Keys\Linux" 
 
 ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
 #on azure entry machine
 mkdrir -p keys
 mv linBox.* keys/
 sudo mkdir -p /etc/docker/certs.d/linBox
-sudo mv ~/keys/linBox.crt /etc/docker/certs.d/linBox:5000/ca.crt
+sudo cp ~/keys/linBox.crt /etc/docker/certs.d/linBox:5000/ca.crt
 
 
 #sudo sh -c "echo ' 192.168.99.100 myregistry.com' >> /etc/hosts"
@@ -22,5 +26,6 @@ sudo docker service create --name smregistry --publish=5000:5000 \
  -e REGISTRY_HTTP_TLS_KEY=/certs/linBox.key \
  registry:latest
 
+ssh linuxadmin@lincent2
 
-docker tag alpine localhost:5000/alpine
+sudo docker push linBox:5000/hellonode:v2
