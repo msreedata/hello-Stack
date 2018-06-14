e508ae2aa0dd6423641f558743426b4780bb0862
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-14 12:24:40 +0200

    ..

--- ansible/configDocker.sh
+++ ansible/configDocker.sh
@@ -4,6 +4,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
 
-sudo docker swarm join --token SWMTKN-1-0qhdyc05d8mk65fvi3t8pip83x4d8701ungby5dsga8rcbhyiu-33ngxetsyum747aab11mwokfw 10.1.1.4:2377
+sudo docker swarm leave -f
+sudo docker swarm join --token SWMTKN-1-4hy9yfngk65hyi9vf5gxttw18idp1vj6ip7mfg3j4oqoxmcl2b-802tw7jup6gx68nxbczxptm0m 10.1.1.16:2377
 #echo "asdfasdf"
 #echo "asdfasdfdeeeeeeee"
