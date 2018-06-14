bbbb55d9752fac6b1944a1e10524c91a32ac313d
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-14 11:04:35 +0200

    ..

--- ansible/configDocker.sh
+++ ansible/configDocker.sh
@@ -1,13 +1,4 @@
----
-- name: Setup Docker
-  hosts: cent
-  gather_facts: yes
-  become: yes
-  become_method: sudo
-
-- name: Transfer and execute a script.
-  hosts: cent
-  tasks:
-
-     - name: Copy and Execute the script 
-       script: ./configDocker.sh
+sudo docker version
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
