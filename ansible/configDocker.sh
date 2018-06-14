38e93ac53c10c18b5c0a1199cff55bcbd4e1c023
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-14 11:19:09 +0200

    ..

--- ansible/configDocker.sh
+++ ansible/configDocker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#sudo docker version
-#sudo systemctl enable docker
-#sudo systemctl start docker
-#sudo systemctl status docker
+sudo docker version
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
 
-echo "asdfasdf"
-echo "asdfasdfdeeeeeeee"
+
+#echo "asdfasdf"
+#echo "asdfasdfdeeeeeeee"
