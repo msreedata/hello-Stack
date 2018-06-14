d76383820356c77909eee94cc89bc7b3c19e94e4
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-14 12:21:39 +0200

    addign worker through ansible sh

--- ansible/configDocker.sh
+++ ansible/configDocker.sh
@@ -4,6 +4,6 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
 
-
+sudo docker swarm join --token SWMTKN-1-0qhdyc05d8mk65fvi3t8pip83x4d8701ungby5dsga8rcbhyiu-33ngxetsyum747aab11mwokfw 10.1.1.4:2377
 #echo "asdfasdf"
 #echo "asdfasdfdeeeeeeee"
