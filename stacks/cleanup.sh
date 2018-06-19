d4d9f22462b34bf839df0a9255b0bdefa1a821ce
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-19 09:26:00 +0200

    hold

--- stacks/cleanup.sh
+++ stacks/cleanup.sh
@@ -1,3 +1,500 @@
 #clear the dangling images
 sudo docker rmi $(sudo docker images -f "dangling=true" -q)
 
+ 565  sudo sh /usr/local/bin/build.sh
+  566  tail /var/log/smbuild.log
+  567  tail /var/log/smbuild.log -n 100
+  568  echo $null > /var/log/smbuild.log
+  569  sudo nano /var/log/smbuild.log
+  570  tail /var/log/smbuild.log -n 100
+  571  sudo sh /usr/local/bin/build.sh
+  572  tail /var/log/smbuild.log -n 100
+  573  sudo crontab -e
+  574  sudo sh /usr/local/bin/build.sh
+  575  tail /var/log/smbuild.log -n 100
+  576  reboot
+  577  sudo reboot
+  578  tail /var/log/smbuild.log -n 100
+  579  tail /var/log/syslog -n 20
+  580  sudo sh /usr/local/bin/build.sh
+  581  tail /var/log/smbuild.log -n 100
+  582  sudo nano /usr/local/bin/build.sh
+  583  sudo crontab -test
+  584  sudo crontab test
+  585  sudo crontab -e
+  586  sudo reboot
+  587  tail /var/log/syslog -n 20
+  588  tail /var/log/smbuild.log -n 100
+  589  sudo crontab -e
+  590  sudo reboot
+  591  tail /var/log/smbuild.log -n 100
+  592  tail /var/log/syslog -n 20
+  593  tail /var/log/smbuild.log -n 100
+  594  tail /var/log/syslog -n 20
+  595  tail /var/log/smbuild.log -n 100
+  596  sudo nano /usr/local/bin/build.sh
+  597  /home/linuxadmin/
+  598  sudo nano /usr/local/bin/build.sh
+  599  rebppt
+  600  sudo reboot
+  601  ls
+  602  ll
+  603  ll .
+  604  cat m
+  605  ls
+  606  cat build.log
+  607  rm m
+  608  rm build.log
+  609  ll
+  610  ls
+  611  rm newname.txt
+  612  sudo /var/local/bin
+  613  ls /var/local/bin
+  614  sh /usr/local/bin/build.sh
+  615  sudo sh /usr/local/bin/build.sh
+  616  ls /var/local/bin
+  617  sudo sh /usr/local/bin/build.sh
+  618  sudo nano /usr/local/bin/build.sh
+  619  sudo sh /usr/local/bin/build.sh
+  620  sh /usr/local/bin/build.sh
+  621  tail /var/log/syslog -n 2
+  622  tail /var/log/syslog -n 200
+  623  tail /var/log/smbuild.log -n 100
+  624  sudo sh /usr/local/bin/build.sh
+  625  tail /var/log/smbuild.log -n 100
+  626  rm /var/log/smbuild.log
+  627  sudo rm /var/log/smbuild.log
+  628  tail /var/log/smbuild.log -n 100
+  629  sudo reboot
+  630  tail /var/log/smbuild.log -n 100
+  631  sudo nano /usr/local/bin/build.sh
+  632  sudo sh /usr/local/bin/build.sh
+  633  tail /var/log/smbuild.log -n 100
+  634  sudo crontab -e
+  635  /bin/bash
+  636  /bin/bash /usr/local/bin/build.sh
+  637  sudo /bin/bash /usr/local/bin/build.sh
+  638  sudo crontab -u root -e
+  639  ll /usr/local/bin
+  640  chmod u+x build.sh
+  641  chmod u+x /usr/local/bin/build.sh
+  642  sudo chmod u+x /usr/local/bin/build.sh
+  643  ll /usr/local/bin
+  644  reboot
+  645  rebootsudo reboot
+  646  sudo reboot
+  647  tail /var/log/smbuild.log -n 100
+  648  tail /var/log/syslog -n 200
+  649  tail /var/log/smbuild.log -n 100
+  650  sudo reboot
+  651  tail /var/log/smbuild.log -n 100
+  652  tail /var/log/syslog -n 200
+  653  tail /var/log/smbuild.log -n 100
+  654  sudo nano /usr/local/bin/build.sh
+  655  sudo crontab -e
+  656  sudo reboot
+  657  ls /var/log
+  658  ls /var/local/
+  659  ls
+  660  ll
+  661  rm ran.txt
+  662  ll
+  663  sudo crontab -e
+  664  tail /var/log/syslog -n 200
+  665  sudo crontab -e
+  666  sudo reboot
+  667  tail /var/log/syslog -n 200
+  668  ls
+  669  ll
+  670  ls
+  671  sudo crontab -e
+  672  ls /user/local/bin/
+  673  ls /usr/local/bin/build.sh
+  674  sudo nano /usr/local/bin/build.sh
+  675  sudo nano /usr/local/bin/test.sh
+  676  sh $$
+  677  $?
+  678  ??
+  679  $!
+  680  sh /usr/local/bin/test.sh
+  681  sudo crontab -e
+  682  sh /usr/local/bin/test.sh
+  683  sudo nano /usr/local/bin/test.sh
+  684  sh /usr/local/bin/test.sh
+  685  ls
+  686  rm test.out
+  687  ls
+  688  sh /usr/local/bin/test.sh
+  689  ls
+  690  rm test.out
+  691  sudo reboot
+  692  ls
+  693  tail /var/log/syslog -n 200
+  694  syslog.appname
+  695  syslog
+  696  /usr/bin/logger
+  697  sudo crontab -e
+  698  sudo reboot
+  699  tail /var/log/syslog -n 200
+  700  logger "asdfasdf"
+  701  tail /var/log/syslog -n 200
+  702  ls
+  703  sudo nano /usr/local/bin/test.sh
+  704  sudo reboot
+  705  ls
+  706  sh /usr/local/bin/test.sh
+  707  sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild
+  708  tail /var/log/syslog -n 200
+  709  tail /var/log/syslog -n 300 | grep SMBuild
+  710  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  711  tail /var/log/syslog -n 300 | grep SMBuild
+  712  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  713  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh ; fi
+  714  sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild
+  715  tail /var/log/syslog -n 300 | grep SMBuild
+  716  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  717  tail /var/log/syslog -n 300 | grep SMBuild
+  718  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh; fi
+  719  ls
+  720  rm test.out
+  721  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  722  ls
+  723  tail /var/log/syslog -n 300 | grep SMBuild
+  724  ls
+  725  rm test.out
+  726  ls
+  727  tail /var/log/syslog -n 300 | grep SMBuild
+  728  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  729  ls
+  730  tail /var/log/syslog -n 300 | grep SMBuild
+  731  ls
+  732  rm test.out
+  733  sudo reboot
+  734  ls
+  735  tail /var/log/syslog -n 300 | grep SMBuild
+  736  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  737  tail /var/log/syslog -n 300 | grep SMBuild
+  738  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  739  tail /var/log/syslog -n 300 | grep SMBuild
+  740  rm test.out
+  741  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  742  tail /var/log/syslog -n 300 | grep SMBuild
+  743  ls
+  744  rm test.out
+  745  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  746  ls
+  747  tail /var/log/syslog -n 300 | grep SMBuild
+  748  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  749  tail /var/log/syslog -n 300 | grep SMBuild
+  750  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh | /usr/bin/logger -t SMBuild; fi
+  751  tail /var/log/syslog -n 300 | grep SMBuild
+  752  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 | /usr/bin/logger -t SMBuild; fi
+  753  tail /var/log/syslog -n 300 | grep SMBuild
+  754  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo adf | /usr/bin/logger -t SMBuild; fi
+  755  tail /var/log/syslog -n 300 | grep SMBuild
+  756  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo adf | /usr/bin/logger -t SMBuild; fi
+  757  tail /var/log/syslog -n 300 | grep SMBuild
+  758  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo adf | /usr/bin/logger -t SMBuild; fi
+  759  tail /var/log/syslog -n 300 | grep SMBuild
+  760  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dadf | /usr/bin/logger -t SMBuild; fi
+  761  tail /var/log/syslog -n 300 | grep SMBuild
+  762  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dadf | /usr/bin/logger -t SMBuild; fi
+  763  tail /var/log/syslog -n 300 | grep SMBuild
+  764  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dddadf | /usr/bin/logger -t SMBuild; fi
+  765  tail /var/log/syslog -n 300 | grep SMBuild
+  766  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dddadf | /usr/bin/logger -t SMBuild; fi
+  767  tail /var/log/syslog -n 300 | grep SMBuild
+  768  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dddadf | /usr/bin/logger -t SMBuild; fi
+  769  tail /var/log/syslog -n 300 | grep SMBuild
+  770  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo dddadf | /usr/bin/logger -t SMBuild; fi
+  771  tail /var/log/syslog -n 300 | grep SMBuild
+  772  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef | /usr/bin/logger -t SMBuild; fi
+  773  tail /var/log/syslog -n 300 | grep SMBuild
+  774  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef | /usr/bin/logger -t SMBuild; fi
+  775  tail /var/log/syslog -n 300 | grep SMBuild
+  776  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef | /usr/bin/logger -t SMBuild; fi
+  777  tail /var/log/syslog -n 300 | grep SMBuild
+  778  ls
+  779  rm test.out
+  780  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef 2>&1 >> /var/log/some.log; fi
+  781  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef 2>&1 >> /home/linuxadmin/some.log; fi
+  782  ls
+  783  cat some.log
+  784  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef 2>&1 >> /home/linuxadmin/some.log; fi
+  785  cat some.log
+  786  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then echo eef 2>&1 >> /home/linuxadmin/some.log; fi
+  787  cat some.log
+  788  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 >> /home/linuxadmin/smlog.log; fi
+  789  ls
+  790  cat smlog.log
+  791  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then sh /usr/local/bin/test.sh 2>&1 >> /home/linuxadmin/smlog.log; fi
+  792  sudo crontab -e
+  793  sudo reboot
+  794  ls
+  795  cat some.log
+  796  rm some.log
+  797  cat some.log
+  798  tail /var/log/syslog -n 300
+  799  sudo reboot
+  800  tail /var/log/syslog -n 300
+  801  cat some.log
+  802  sudo crontab -e
+  803  sudo reboot
+  804  cat some.log
+  805  tail /var/log/syslog -n 300
+  806  sudo crontab -e
+  807  sudo reboot
+  808  sudo tail -f /var/log/cron.log
+  809  tail /var/log/syslog -n 300
+  810  tail /var/log/syslog -n 1200
+  811  tail /var/log/syslog -n 1200 | less
+  812  sudo crontab -e
+  813  sudo reboot
+  814  grep cron /var/log/syslog | less
+  815  tail /var/log/syslog -n 300
+  816  ls
+  817  cat smlog.log
+  818  rm smlog.log
+  819  rm test.out
+  820  sudo reboot
+  821  ls
+  822  tail /var/log/syslog -n 300
+  823  sudo crontab -e
+  824  ls
+  825  sudo reboot
+  826  ls
+  827  tail /var/log/syslog -n 300
+  828  tail /var/log/syslog | grep SMBuild
+  829  cat /var/log/syslog | grep SMBuild
+  830  sudo crontab -e
+  831  if (echo "$(blkid /dev/sr0)" | grep -q cidata); then /bin/sh /usr/local/bin/test.sh 2>&1 >> /home/linuxadmin/smlog.log; fi
+  832  ls
+  833  cat /usr/local/bin/test.sh
+  834  ll
+  835  cat /usr/local/bin/test.sh
+  836  rm test.out
+  837  rm smlog.log
+  838  ls
+  839  sudo crontab -e
+  840  sudo reboot
+  841  ls
+  842  cat /var/log/syslog | grep SMBuild
+  843  cat /var/log/syslog | grep test.sh
+  844  ls /home/linuxadmin/
+  845  ll /home/linuxadmin/
+  846  sudo crontab -e
+  847  sudo nano /usr/local/bin/build.sh
+  848  sudo reboot
+  849  ls /home/linuxadmin
+  850  cat /var/log/syslog | grep SMBuild
+  851  ls /home/linuxadmin
+  852  ls /var/log
+  853  ll /var/log
+  854  cat /var/log/smbuild.log
+  855  sudo nano /usr/local/bin/build.sh
+  856  ll
+  857  cat /var/log/syslog | grep SMBuild
+  858  ll
+  859  cat /var/log/syslog | grep SMBuild
+  860  cat /var/log/smbuild.log
+  861  ls /var/log/smbuild.log
+  862  ll /var/log/smbuild.log
+  863  chmod 777 /var/log/smbuild.log
+  864  sudo chmod 777 /var/log/smbuild.log
+  865  ll /var/log/smbuild.log
+  866  sudo chmod 644 /var/log/smbuild.log
+  867  ll /var/log/smbuild.log
+  868  sudo chmod 774 /var/log/smbuild.log
+  869  ll /var/log/smbuild.log
+  870  sudo chmod 664 /var/log/smbuild.log
+  871  ll /var/log/smbuild.log
+  872  sudo reboot
+  873  ll /var/log/smbuild.log
+  874  cat /var/log/syslog | grep SMBuild
+  875  less /var/log/smbuild.log
+  876  sudo nano /usr/local/bin/build.sh
+  877  sudo reboot
+  878  cat /var/log/syslog | grep SMBuild
+  879  time
+  880  date
+  881  cat /var/log/syslog | grep SMBuild
+  882  less /var/log/smbuild.log
+  883  ls
+  884  ll
+  885  sudo nano /usr/local/bin/build.sh
+  886  sudo crontab -e
+  887  sudo nano /usr/local/bin/build.sh
+  888  sudo reboot
+  889  ll
+  890  less /var/log/smbuild.log
+  891  ll /var/log/smbuild.log
+  892  less /var/log/smbuild.log
+  893  tail /var/log/syslog -n 300
+  894  /bin/bash /usr/local/bin/build.sh
+  895  sudo nano /usr/local/bin/build.sh
+  896  sudo /bin/bash /usr/local/bin/build.sh
+  897  ll
+  898  ls
+  899  rm -rf test/
+  900  ls
+  901  rm ran.txt
+  902  ll
+  903  rm washere
+  904  ll /var/log
+  905  cat /var/log/smbuild.log
+  906  sudo /bin/bash /usr/local/bin/build.sh
+  907  sudo crontab -e
+  908  sudo reboot
+  909  which echo
+  910  sudo nano /usr/local/bin/build.sh
+  911  sudo reboot
+  912  ls
+  913  tail /var/log/syslog -n 300
+  914  sudo /bin/bash /usr/local/bin/build.sh
+  915  ls
+  916  rm -rf test/
+  917  rm -f washere
+  918  rm -f ran.txt
+  919  ls
+  920  sudo reboot
+  921  ls
+  922  sudo /bin/bash /usr/local/bin/build.sh
+  923  ls
+  924  rm -f ran.txt
+  925  rm -f washere
+  926  rm -rf test/
+  927  ls
+  928  sudo nano /usr/local/bin/build.sh
+  929  sudo crontab -e
+  930  cd /tmp
+  931  ls
+  932  sudo reboot
+  933  cd /tmp
+  934  ls
+  935  less joblog.txt
+  936  sudo crontab -e
+  937  lbkid
+  938  less joblog.txt
+  939  which blkid
+  940  sudo crontab -e
+  941  which blkid
+  942  sudo reboot
+  943  less /temp/joblog.txt
+  944  cd /tmp
+  945  ls
+  946  less joblog.txt
+  947  ls
+  948  cd ~
+  949  ls
+  950  rm -rf test/
+  951  rm -f washere
+  952  rm -f ran.txt
+  953  ls
+  954  sudo reboot
+  955  ping linuxlab2
+  956  shutdown now
+  957  sudo shutdown now
+  958  passwd
+  959  ls
+  960  ll
+  961  rm *
+  962  rm -rf test/
+  963  logout
+  964  exit
+  965  sudo shutdown
+  966  sudo shutdown now
+  967  ping dreamznas
+  968  ls /mnt/dvd
+  969  ls
+  970  sh /mnt/dvd
+  971  sh /mnt/dvd/build.sh
+  972  rm -rf *
+  973  sudo sh /mnt/dvd/build.sh
+  974  ping dreamznas
+  975  sudo apt upgrade
+  976  sudo apt update
+  977  shutdown
+  978  shutdown now
+  979  history
+  980  exit
+  981  ssh-keygen -t rsa -b 4096 -N '' -C "msreedata@gmail.com" -f ~/.ssh/msreedata_rsa
+  982  hostname
+  983  scp registry.crt smlin.westeurope.cloudapp.azure.com
+  984  ssh LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+  985  ls
+  986  ssh -i ~/.ssh/msreedata_rsa smlin.westeurope.cloudapp.azure.com
+  987  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+  988  ip a
+  989  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+  990  history 200
+  991  ls ~/.ssh
+  992  ls ~/.ssh -hal
+  993  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+  994  history 200
+  995  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+  996  9  nano ~/.ssh/authorized_keys
+  997  openssl req -newkey rsa:4096 -nodes -sha256   -keyout registry.key -x509 -days 365 \
+  998  openssl req -newkey rsa:4096 -nodes -sha256 -keyout registry.key -x509 -days 365 -out registry.crt
+  999  ls
+ 1000  scp registry.crt -i ~/.ssh/msreedata_rsa smlin.westeurope.cloudapp.azure.com
+ 1001  scp registry.crt -i ~/.ssh/msreedata_rsa smlin.westeurope.cloudapp.azure.com:
+ 1002  scp registry.crt -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+ 1003  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1004  scp registry.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+ 1005  cat registry.crt
+ 1006  cat registry.key
+ 1007  openssl x509 -in registry.crt -noout -text
+ 1008  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/registry.smlabs.key -x509 -days 365 -out ~/keys/registry.smlabs.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=registry.smlabs.com"
+ 1009  mkdir keys
+ 1010  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/registry.smlabs.key -x509 -days 365 -out ~/keys/registry.smlabs.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=registry.smlabs.com"
+ 1011  ls keys/
+ 1012  ls
+ 1013  openssl x509 -in ~/keys/registry.smlabs.crt -noout -text
+ 1014  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/registry.smlabs.key -x509 -days 365 -out ~/keys/registry.smlabs.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=registry.smlabs.com"
+ 1015  openssl x509 -in ~/keys/registry.smlabs.crt -noout -text
+ 1016  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/registry.smlabs.key -x509 -days 365 -out ~/keys/registry.smlabs.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=registry.smlabs.com"
+ 1017  openssl x509 -in ~/keys/registry.smlabs.crt -noout -text
+ 1018  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/registry.smlabs.key -x509 -days 365 -out ~/keys/registry.smlabs.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=registry.smlabs.com"
+ 1019  scp ~/keys/registry.smlabs.crt ~/keys/registry.smlabs.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1020  scp ~/keys/registry.smlabs.crt ~/keys/registry.smlabs.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+ 1021  ls
+ 1022  scp ~/keys/ LinuxAdmin@smlin.westeurope.cloudapp.azure.com:~/
+ 1023  scp ~/keys/* LinuxAdmin@smlin.westeurope.cloudapp.azure.com:~/
+ 1024  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1025  #linBox CN
+ 1026  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/linBox.key -x509 -days 365 -out ~/keys/linBox.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=linBox.com"
+ 1027  #upload the files to azure #registy.smlab.com
+ 1028  scp ~/keys/linBox.crt ~/keys/linBox.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+ 1029  mv linBox.* keys/
+ 1030  mkdir -p /etc/docker/certs.d/linBox
+ 1031  mv linBox.* keys/
+ 1032  sudo mkdir -p /etc/docker/certs.d/linBox
+ 1033  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1034  ls  /etc/docker/certs.d
+ 1035  ls  /etc/docker/certs.d/linBox/
+ 1036  ls
+ 1037  ls keys/
+ 1038  ls
+ 1039  docker swarm init
+ 1040  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1041  scp ~/keys/linBox.crt ~/keys/linBox.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:~/keys
+ 1042  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1043  ls keys/
+ 1044  rm ~/keys -rf
+ 1045  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/linBox.key -x509 -days 365 -out ~/keys/linBox.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=linBox"
+ 1046  ls
+ 1047  mkdir keys
+ 1048  rm ~/keys/* -rf
+ 1049  mkdir ~/keys
+ 1050  openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/keys/linBox.key -x509 -days 365 -out ~/keys/linBox.crt -subj "/C=CH/ST=Vaud/L=Lausanne/O=SMLabs/OU=Org/CN=linBox"
+ 1051  ls keys/
+ 1052  #upload the files to azure #registy.smlabs.com
+ 1053  scp ~/keys/linBox.crt ~/keys/linBox.key LinuxAdmin@smlin.westeurope.cloudapp.azure.com:
+ 1054  ssh -i ~/.ssh/msreedata_rsa LinuxAdmin@smlin.westeurope.cloudapp.azure.com
+ 1055  ip a
+ 1056  ping dreamznas
+ 1057  exit
+ 1058  ip a
+ 1059  exit
+ 1060  ip a
+ 1061  exit
